9dd8d1ab19d8e8e9629715c0be774b9ac9be4980
Olamide <lamibanu@gmail.com>
2023-01-05 17:19:47 -0800

    push

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,27 +4,27 @@ if __name__ == "__main__":
     from sys import argv
     if len(argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        return 1
+        exit(1)
     a = int(argv[1])
     operator = argv[2]
     b = int(argv[3])
     if operator == "+":
         result = add(a, b)
-        print('{} {} {} = {}'.format(a, operator, b, result)
-#        return (0)
+        print('{} {} {} = {}'.format(a, operator, b, result))
+        
     elif operator == "-":
         result = sub(a, b)
-        print('{} {} {} = {}'.format(a, operator, b, result)
-#        return (0)
+        print('{} {} {} = {}'.format(a, operator, b, result))
+
     elif operator == "*":
         result = mul(a, b)
-        print('{} {} {} = {}'.format(a, operator, b, result)
-#        return (0)
+        print('{} {} {} = {}'.format(a, operator, b, result))
+
     elif operator == "/":
         result = div(a, b)
-        print('{} {} {} = {}'.format(a, operator, b, result)
-#        return (0)      
+        print('{} {} {} = {}'.format(a, operator, b, result))
+
     else:
         print('Unknown operator. Available operators: +, -, * and /')
-        return (1)
+        exit(1)
     

17c8a94dfae227b103c76e3f12583123939ad507
Olamide <lamibanu@gmail.com>
2023-01-08 12:08:02 -0800

    push

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if idx < 0:
-       return my_list
+        return my_list
     list_len = len(my_list)
-    if idx > list_len:
+    if idx >= list_len:
         return my_list
     del my_list[idx]
     return my_list

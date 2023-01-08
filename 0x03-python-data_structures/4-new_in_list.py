5aec17aac872aa2850f8162b32af9995b3974ff3
Olamide <lamibanu@gmail.com>
2023-01-07 20:59:47 -0800

    push

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    list_len = len(my_list)
+    if idx > list_len:
+        return my_list
+    newlist = my_list.copy()
+    newlist[idx] = element
+    return newlist
+    

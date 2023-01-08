7a36c72b09ca5691c9361ef717f8ac8592399cb1
Olamide <lamibanu@gmail.com>
2023-01-07 19:29:47 -0800

    push

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,8 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    lenlist = len(my_list)
-    elif idx >= lenlist:
+    list_len = len(my_list)
+    if idx >= list_len:
         return my_list
     my_list[idx] = element
     return my_list

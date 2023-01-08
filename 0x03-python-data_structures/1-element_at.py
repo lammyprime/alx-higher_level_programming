3361828e6e0cd6ee8c317173159a45500e383b52
Olamide <lamibanu@gmail.com>
2023-01-07 19:21:47 -0800

    push

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return (None)
-    elif idx > len(my_list):
-        return (None)
+    list_len = len(my_list) - 1
+    if idx > list_len:
+        return None
     return my_list[idx]

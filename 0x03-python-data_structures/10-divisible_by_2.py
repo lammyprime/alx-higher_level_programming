48a5dd09806299e28a9de7817c6a5bfc93cd0045
Olamide <lamibanu@gmail.com>
2023-01-08 12:31:26 -0800

    push

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,9 +3,9 @@ def divisible_by_2(my_list=[]):
     lenlist = len(my_list)
     if lenlist != 0:
         newlist = []
-    for i in my_list:
-        if i % 2 == 0:
-            newlist += [True]
-        else:
-            newlist += [False]
-    return newlist    
+        for i in my_list:
+            if i % 2 == 0:
+                newlist += [True]
+            else:
+                newlist += [False]
+        return newlist    

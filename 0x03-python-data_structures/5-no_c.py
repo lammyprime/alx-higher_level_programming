08c5ffcbe590fbc2a2a44c523966e26982a4ef35
Olamide <lamibanu@gmail.com>
2023-01-07 21:43:46 -0800

    push

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-     string = ''
-     for char in my_string:
-          if char != "C" and char != "c":
-               string += char
-     return string
+    string = ''
+    for char in my_string:
+        if char != "C" and char != "c":
+            string += char
+    return string

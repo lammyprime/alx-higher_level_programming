185f78292bd1ea7d10fbe61876a137e35f7f4946
Olamide <lamibanu@gmail.com>
2023-01-11 11:29:23 -0800

    push

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    upd = {key: "value"}
-    a_dictionary.update(upd)
+    a_dictionary[key] = value
     return (a_dictionary)

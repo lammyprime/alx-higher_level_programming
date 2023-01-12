1d7749d9c230beae181a11f2387f8215ef0ea4e3
Olamide <lamibanu@gmail.com>
2023-01-11 17:11:05 -0800

    push

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string type(roman_string) != str:
+        return 0
+    for i in range(1, 4000):
+        return roman_string[i]
